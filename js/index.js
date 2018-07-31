0952c5322af0be11f6c5f93b81024472a2fb2f0f
jaime <design.jumi@gmail.com>
2018-07-30 20:54:06 -0700

    Fixed tile text not showing issue

--- js/index.js
+++ js/index.js
@@ -1,32 +1,3 @@
-// Auto complete code for Animate.css 
-$.fn.extend({
-    animateCss: function (animationName, callback) {
-        var animationEnd = (function (el) {
-            var animations = {
-                animation: 'animationend',
-                OAnimation: 'oAnimationEnd',
-                MozAnimation: 'mozAnimationEnd',
-                WebkitAnimation: 'webkitAnimationEnd',
-            };
-
-            for (var t in animations) {
-                if (el.style[t] !== undefined) {
-                    return animations[t];
-                }
-            }
-        })(document.createElement('div'));
-
-        this.addClass('animated ' + animationName).one(animationEnd, function () {
-            $(this).removeClass('animated ' + animationName);
-
-            if (typeof callback === 'function') callback();
-        });
-
-        return this;
-    },
-
-});
-
 $('#show').on('click', function () {
     $('.card-reveal').slideToggle('slow');
 });
