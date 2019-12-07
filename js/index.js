5646bf904e4f66bf295a143bc1ae0c80be378458
Jae Kim <jaewookk@google.com>
2019-12-06 21:53:25 -0800

    Increase the text animation

--- js/index.js
+++ js/index.js
@@ -1,51 +1,3 @@
-// // bodymovin animation
-// var animation = bodymovin.loadAnimation({
-//     container: document.getElementById('bm'),
-//     renderer: 'svg',
-//     loop: true,
-//     autoplay: true,
-//     path: 'data.json'
-// })
-
-// const words = ["UX DESIGNER", "PROBLEM-SOLVER", ""];
-// let i = 0;
-// let timer;
-
-// function typingEffect() {
-// 	let word = words[i].split("");
-// 	var loopTyping = function() {
-// 		if (word.length > 0) {
-// 			document.getElementById('word').innerHTML += word.shift();
-// 		} else {
-// 			deletingEffect();
-// 			return false;
-// 		};
-// 		timer = setTimeout(loopTyping, 100);
-// 	};
-// 	loopTyping();
-// };
-
-// function deletingEffect() {
-// 	let word = words[i].split("");
-// 	var loopDeleting = function() {
-// 		if (word.length > 0) {
-// 			word.pop();
-// 			document.getElementById('word').innerHTML = word.join("");
-// 		} else {
-// 			if (words.length > (i + 1)) {
-// 				i++;
-// 			} else {
-// 				i = 0;
-// 			};
-// 			typingEffect();
-// 			return false;
-// 		};
-// 		timer = setTimeout(loopDeleting, 150);
-// 	};
-// 	loopDeleting();
-// };
-
-// typingEffect();
 
 // List of sentences
 var _CONTENT = ["UX DESIGNER", "PROBLEM-SOLVER", "VISUAL DESIGNER", "PIXEL PERFECTER"];
@@ -96,10 +48,10 @@ function Delete() {
 
 		// Start to display the next sentence after some time
 		setTimeout(function () {
-			_INTERVAL_VAL = setInterval(Type, 100);
+			_INTERVAL_VAL = setInterval(Type, 40);
 		}, 200);
 	}
 }
 
 // Start the typing effect on load
-_INTERVAL_VAL = setInterval(Type, 100);
+_INTERVAL_VAL = setInterval(Type, 40);
